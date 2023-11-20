27d1ed93fc99bbd6c31148bb1f845073348cfc44
kapila-dhilipan <120449602+kapila-dhilipan@users.noreply.github.com>
2023-11-20 10:51:28 +0530

    Update guess-no.py

--- guess-no.py
+++ guess-no.py
@@ -1,7 +1,7 @@
 import random
 number = random.randint(1, 10)
 
-player_name = input("Hello, What's your name?")
+player_name = input("Hello, What's your name? good morning")
 no_of_guesses = 0
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
 
